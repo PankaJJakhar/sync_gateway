1da7b4d37f2a9022d535574003010f72e9b51d65
Chris Anderson <jchris@couchbase.com>
2013-12-22 19:07:32 -0800

    work on sync ui

--- utils/assets/dbinfo.js
+++ utils/assets/dbinfo.js
@@ -37,13 +37,25 @@ window.DbInfoPage = React.createClass({
   }
 })
 
+var examples = {
+  "basic" : "function(doc){\n  channel(doc.channels)\n}"
+}
+
+
 var SyncFunctionView = React.createClass({
+  handleExampleClick : function(){
+    console.log("handleExampleClick", this.refs.syncCode.getDOMNode())
+    var ta = this.refs.syncCode.getDOMNode()
+    ta.value = examples["basic"]
+    //  = examples["basic"];
+  },
   render : function() {
     var docID = "84DEC4C6-D287-4062-8C9B-5692A2CA8929"
     return <div className="SyncFunctionView">
     <h3>Sync Function</h3>
       <p>This code determines Sync Gateway application behavior. It can validate document updates, route documents to channels, and grant access to users and groups to read from channels. For more information <a href="http://docs.couchbase.com/sync-gateway/#sync-function-api">see the Sync Function documentation.</a></p>
-      <pre><code>{this.props.code}</code></pre>
+      <textarea ref="syncCode" value={this.props.code}/>
+      <p>Examples: <a onClick={this.handleExampleClick}>default</a></p>
       <h3>Preview Sync Results</h3>
       <p>This preview shows the channel mapping and access control output of the sync function based on a document in your database.</p>
       <p><a onClick={function(){}}>Select a random document.</a>{" "}
